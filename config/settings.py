bc9fce0ea7f2e8b37cd1c9047df908b069f164f6
ByteNadeem <nadeemzachav@hotmail.com>
2025-09-08 16:33:22 +0100

    set DEBUG to False for production readiness

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ if os.path.isfile("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =["deanery-11c659a713eb.herokuapp.com", '127.0.0.1'] 
 
