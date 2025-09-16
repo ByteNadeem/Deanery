67d6dfba0b877e5bac0157afa9620deb340e7f37
humanauction <t.a.harris@gmail.com>
2025-09-16 13:20:11 +0100

    Add: settings: allowed hosts Google Maps API key; Add: map home template, view, initial js

--- map/views.py
+++ map/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
 
 def map_home(request):
-    return render(request, 'map/map_home.html')
+    return render(request, 'map/map_home.html', {
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY
+    })
