b0b1e71e7b3180249e05e6ab04ccb24bfc6dd04e
ByteNadeem <nadeemzachav@hotmail.com>
2025-09-17 12:02:41 +0100

    fix: update static file paths in settings.py

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'theme/static'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
