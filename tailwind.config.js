f1cb6719acc7d35cf37851e4368a86861f943a98
humanauction <t.a.harris@gmail.com>
2025-09-26 12:38:15 +0100

    add: update base and index templates with new layout and modal functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,32 @@ module.exports = {
         './**/*.py',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                asparagus: '#7DA068',
+                darkred: '#880C00',
+                black: '#000000',
+                pistachio: '#98C880',
+                black2: '#050804',
+            },
+        },
     },
-    plugins: [],
-};
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#7DA068",      // asparagus
+                    secondary: "#98C880",    // pistachio
+                    accent: "#880C00",       // dark red
+                    neutral: "#050804",      // black2
+                    "base-100": "#ffffff",
+                    info: "#2D3D26",
+                    success: "#648252",
+                    warning: "#FFD700",
+                    error: "#D41200",
+                },
+            },
+        ],
+    },
+    plugins: [require('daisyui')],
+}
