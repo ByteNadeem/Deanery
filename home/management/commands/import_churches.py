d178464a5c9d9c738ef8ed6db4f79aa3c2216e27
humanauction <t.a.harris@gmail.com>
2025-10-17 10:44:56 +0100

    Add: import_churches.py, Command to import churches from CSV file.

---
+++ home/management/commands/import_churches.py
@@ -0,0 +1,39 @@
+import csv
+import os
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from home.models import Church
+
+
+class Command(BaseCommand):
+    help = 'Import churches from csv'
+
+    def handle(self, *args, **kwargs):
+        csv_path = os.path.join(
+            settings.BASE_DIR,
+            'docs',
+            'docs',
+            'NorthCarnmarthDeaneryLocations.csv',
+        )
+        with open(csv_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            count = 0
+            for row in reader:
+                lat = row.get('Latitude')
+                lon = row.get('Longitude')
+                latitude = float(lat) if lat else None
+                longitude = float(lon) if lon else None
+                Church.objects.update_or_create(
+                    name=row['Name'],
+                    defaults={
+                        'address': row['Location'],
+                        'postcode': row['Postcode'],
+                        'latitude': latitude,
+                        'longitude': longitude,
+                        'contact': '',  # Add contact if available in CSV
+                    }
+                )
+                count += 1
+            self.stdout.write(
+                self.style.SUCCESS(f'Imported Churches: {count}')
+            )
